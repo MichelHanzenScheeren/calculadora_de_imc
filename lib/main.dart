aa0b6ceefea15d34c98a4abfa1e39dedd913e9fa
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2020-03-31 13:52:27 -0300

    Adição do botão de calcular e texto dos resultados.
    Todo o container da coluna foi também inserido em um widget de Scroll e add paddin.

--- lib/main.dart
+++ lib/main.dart
@@ -24,36 +24,55 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("Calculadora de IMC"),
-        backgroundColor: Colors.deepOrange,
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.refresh), onPressed: () {})
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_outline, color: Colors.deepOrange, size: 120),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Peso (kg)",
-                labelStyle: TextStyle(color: Colors.deepOrange)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.deepOrange, fontSize: 25),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          backgroundColor: Colors.deepOrange,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.refresh), onPressed: () {})
+          ],
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline, color: Colors.deepOrange, size: 120),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.deepOrange)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.deepOrange, fontSize: 25),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.deepOrange)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.deepOrange, fontSize: 25),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10, top: 10),
+                child: Container(
+                  height: 50,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    color: Colors.deepOrange,
+                    child: Text("Calcular",
+                        style: TextStyle(color: Colors.white, fontSize: 25)),
+                  ),
+                ),
+              ),
+              Text(
+                "Resultado:",
+                style: TextStyle(color: Colors.deepOrange, fontSize: 25),
+                textAlign: TextAlign.center,
+              )
+            ],
           ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Altura (cm)",
-                labelStyle: TextStyle(color: Colors.deepOrange)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.deepOrange, fontSize: 25),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
